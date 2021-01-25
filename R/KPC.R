5fd70258b9d6344e297e1a39945cb9d8f6d3a604
Luis Iglesias Martinez <luis.iglesiasmartinez@ucd.ie>
2021-01-25 16:38:09 +0000

    Use only real eigenvalues in KPC function.

--- R/KPC.R
+++ R/KPC.R
@@ -8,17 +8,17 @@ KPC = function(K,thr){
   # Perform the eigen decomposition.
   Ei = eigen(K)
   # Ei is a structure with eigen values and eigen vectors.
-  Pass = (Ei$values>thr)
+  Pass = (Re(Ei$values)>thr)
   # Project the eigen-vectors on the Kernel matrix.
   if (sum(Pass) > 0){
-      KPC = K%*%Ei$vectors[,Pass]
+      KPC = K%*%Re(Ei$vectors[,Pass])
        # Set their euclidean norm to one.
       for (i in 1:(dim(KPC)[2])){
       KPC[,i] = KPC[,i] / sum(KPC[,i]^2)^(1/2)
       }
       return(KPC)
   } else {
-    print("No Eigen-values lower than thr. You can try using a lower thr")
+    stop("No Eigen-values lower than thr. You can try using a lower thr")
   }
 
 
